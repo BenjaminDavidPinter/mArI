76541ff25228b7cca296a95051555843f377a9f6
Benjamin Pinter <72715672+BenjaminDavidPinter@users.noreply.github.com>
2024-06-28 22:41:53 -0400

    Cleaning up so that it builds

--- mArI.Lib/Services/Government.cs
+++ mArI.Lib/Services/Government.cs
@@ -4,10 +4,5 @@ namespace mArI.Services;
 
 public class Government
 {
-    public CommitteeDecision AskCommittee(List<Assistant> assistantsDefs,
-    List<AssistantFile> assistantFiles,
-    string Question)
-    {
 
-    }
 }
